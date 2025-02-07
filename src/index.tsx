ae287efbae47b9167745fd6aeca4b67aacc02cb0
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-02-07 15:44:30 +0200

    set up continued

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM, {Container} from 'react-dom/client';
+
+const App: React.FC = ()=>{
+    const a: string = 'world';
+    return <div>Hello world! {a}</div>
+};
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as Container
+);
+
+root.render(<App/>);
