f0de92f737770db5265d6c34c2a6007db20a0e0a
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-03-06 00:12:58 +0200

    mini button component: icons search and arrow

--- src/MiniButton/MiniButton.tsx
+++ src/MiniButton/MiniButton.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './style.css';
-import classNames from 'classnames';
-import searchIcon from '../../static/search.png';
-export const SearchButton: React.FC = ()=>{
-    return <button className={styles.search}><img src={searchIcon}/></button>;
+
+type ImageProp ={
+    imagePath: string;
 }
+export const MiniButton: React.FC<ImageProp> = ({imagePath})=>{
+    return <button className={styles.miniButton}><img src={imagePath}/></button>;
+};
--- src/stories/MiniButton.stories.tsx
+++ src/stories/MiniButton.stories.tsx
@@ -1,14 +1,25 @@
 import type {Meta, StoryObj} from '@storybook/react';
+import search from '../../images/search.png';
+import arrow from '../../images/down-arrow.png';
 
-import {SearchButton} from "../SearchButton/SearchButton";
+import {MiniButton} from '../MiniButton/MiniButton';
 
-const meta : Meta<typeof  SearchButton>={
-    component: SearchButton,
-    title: 'SearchButton',
+const meta : Meta<typeof  MiniButton>={
+    component: MiniButton,
+    title: 'MiniButton',
     tags: ['autodocs']
 
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>
-export const Primary: Story ={};
+export const Search: Story ={
+    args: {
+        imagePath: search
+    }
+};
+export const Arrow: Story ={
+    args: {
+        imagePath: arrow
+    }
+};
