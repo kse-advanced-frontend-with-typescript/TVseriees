f653044b0c91405ee4f482203c71649a9e3f62ed
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-04-13 06:13:40 +0300

    added test for createSeriesOperations

--- src/modules/createSeriesOperations.ts
+++ src/modules/createSeriesOperations.ts
@@ -2,7 +2,7 @@ import {Collection, State} from '../types';
 import {Dispatch, SetStateAction} from 'react';
 import {AppContext} from '../context';
 
-type SeriesOperations = {
+export type SeriesOperations = {
     deleteSerie: (serie_id: number, collection: Collection) => Promise<void>;
     addSerie: (serie_id: number, collection: Collection) => Promise<void>;
     deleteAll: (collection: Collection) => Promise<void>;
@@ -22,33 +22,35 @@ export const createSeriesOperations = <T extends State>(
             setState(prev => ({ ...prev, loading: false, error: true } as T));
         }
     };
+    const deleteSerie = async (serie_id: number, collection: Collection): Promise<void> => {
+        await executeOperation(async () => {
+            if (!context.user?._id) throw new Error('User must be logged in to delete series');
+            const _id = context.userCollections[collection].get(serie_id);
+            if (!_id) throw new Error(`Series with ID ${serie_id} not found in collection`);
+            await context.userAPI.removeSerie(_id, collection);
+            context.userCollections[collection].delete(serie_id);
+        });
+    };
+    const addSerie = async (serie_id: number, collection: Collection): Promise<void> => {
+        await executeOperation(async () => {
+            if (!context.user?._id) throw new Error('User must be logged in to add series');
+            const _id: string = await context.userAPI.addSerie(context.user._id, serie_id, collection);
+            context.userCollections[collection].set(serie_id, _id);
+        });
+    };
+    const deleteAll = async (collection: Collection): Promise<void> => {
+        await executeOperation(async () => {
+            if (!context.user?._id) throw new Error('User must be logged in to delete series');
+            await context.userAPI.removeAll(Array.from(context.userCollections[collection].values()), collection);
+            context.userCollections[collection].clear();
+        });
+    };
 
     return {
-        deleteSerie: async (serie_id: number, collection: Collection): Promise<void> => {
-            await executeOperation(async () => {
-                const _id = context.userCollections[collection].get(serie_id);
-                if (!_id) throw new Error(`Series with ID ${serie_id} not found in collection`);
-                await context.userAPI.removeSerie(_id, collection);
-                context.userCollections[collection].delete(serie_id);
-            });
-        },
-
-        addSerie: async (serie_id: number, collection: Collection): Promise<void> => {
-            await executeOperation(async () => {
-                if (!context.user?._id) throw new Error('User must be logged in to add series');
-                const _id: string = await context.userAPI.addSerie(context.user._id, serie_id, collection);
-                context.userCollections[collection].set(serie_id, _id);
-            });
-        },
-
-        deleteAll: async (collection: Collection): Promise<void> => {
-            await executeOperation(async () => {
-                await context.userAPI.removeAll(
-                    Array.from(context.userCollections[collection].values()),
-                    collection
-                );
-                context.userCollections[collection] = new Map();
-            });
-        }
+        deleteSerie,
+        deleteAll,
+        addSerie
     };
+
+
 };
