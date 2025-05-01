59a4c1430bb571f25bdc6f92a1984c9ae129f3bf
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-05-01 14:09:21 +0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ module.exports = {
                         loader: 'file-loader',
                     },
                 ],
+                options: {
+                    publicPath: process.env.BASE_URL ?? '/'
+                }
             }
         ]
     },
